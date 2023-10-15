2dbfae9d14b4e46bf350997d7d8a6afe7fcd7bc8
Hugo Carvalho <hhac@protonmail.com>
2023-10-15 18:23:40 +0100

    Defined the default page layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "$lib/global.css";
+    import Header from "$lib/header.svelte";
+    import Footer from "$lib/footer.svelte";
+</script>
+
+<Header />
+
+<main class="max-width">
+    <slot />
+</main>
+
+<Footer />
+
+<style>
+    main {
+        margin: 0 auto 50px auto;
+    }
+</style>
